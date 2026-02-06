50bb566a2af62fa11da9e1d898ed284eeb69214f
Garrett-Webster <garrettreeds@gmail.com>
2026-02-05 20:46:50 -0700

    Port the register page.

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 export function Register() {
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>register displayed here</div>
+        <main>
+            <div className="padding"></div>
+            <div className="credentials-box">
+                <h3>Register</h3>
+                <form>
+                    <label htmlFor="name" className="field">
+                        Username
+                        <input type="text" id="name" placeholder="Your username"/>
+                    </label>
+                    <label htmlFor="password" className="field">
+                        Password
+                        <input type="text" id="password" placeholder="Your password"/>
+                    </label>
+                    <label htmlFor="passwordRepeat" className="field">
+                        Enter password again
+                        <input type="text" id="passwordRepeat" placeholder="Your password"/>
+                    </label>
+                    <input type="submit" value="Register"/>
+                    <NavLink to="/login">Already have account?</NavLink>
+                </form>
+            </div>
+            <div className="padding"></div>
         </main>
     );
 }
