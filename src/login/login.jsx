bea11f568a780585c23458813828836f8d4a2f19
Garrett-Webster <garrettreeds@gmail.com>
2026-02-05 20:13:27 -0700

    Port the login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import './login.css';
+import {NavLink} from "react-router-dom";
 
 export function Login() {
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>login displayed here</div>
+        <main>
+            <div className="padding"></div>
+            <div className="credentials-box">
+                <h3>Login</h3>
+                <form>
+                    <label htmlFor="name" className="field">
+                        Username
+                        <input type="text" id="name" placeholder="Your username"/>
+                    </label>
+                    <label htmlFor="password" className="field">
+                        Password
+                        <input type="text" id="password" placeholder="Your password"/>
+                    </label>
+                    <input type="submit" value="Login"/>
+                    <NavLink to="register">Create account</NavLink>
+                </form>
+
+            </div>
+            <div className="padding"></div>
         </main>
     );
 }
