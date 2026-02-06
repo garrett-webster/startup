e4465dd4c1b52e3a4c9fdab376691b07029527d5
Garrett-Webster <garrettreeds@gmail.com>
2026-02-05 19:57:59 -0700

    Add stubs for the components.

---
+++ src/edit/edit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Login() {
+    return (
+        <main className="container-fluid bg-secondary text-center">
+            <div>edit displayed here</div>
+        </main>
+    );
+}
---
+++ src/register/register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Login() {
+    return (
+        <main className="container-fluid bg-secondary text-center">
+            <div>register displayed here</div>
+        </main>
+    );
+}
