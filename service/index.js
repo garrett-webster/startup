a73b12b98b17fffd881cb6b13304fca8b45cd923
Garrett-Webster <garrettreeds@gmail.com>
2026-03-12 18:56:59 -0600

    Add the static file serving.

--- service/index.js
+++ service/index.js
@@ -7,6 +7,7 @@ const {updateEventInfoHandler, getEventInfoHandler} = require("./handlers/eventI
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 app.use(cookieParser());
 
 const server = http.createServer(app);
