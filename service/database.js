40b43d39b96da9b4252580f38e40d94f07cfe544
Garrett-Webster <garrettreeds@gmail.com>
2026-03-24 19:14:37 -0600

    Convert eventInfoService.js to use mongodb

--- service/database.js
+++ service/database.js
@@ -5,6 +5,7 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('whenify');
 const userCollection = db.collection('user');
+const eventInfoCollection = db.collection('eventInfo')
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -29,8 +30,18 @@ async function updateUser(field, value, user) {
     await userCollection.updateOne({ [field]: value}, { $set: user})
 }
 
+async function fetchEventInfo() {
+    return eventInfoCollection.findOne({});
+}
+
+async function setEventInfo(info) {
+    await eventInfoCollection.replaceOne({}, info, { upsert: true });
+}
+
 module.exports = {
     getUser,
     insertUser,
-    updateUser
+    updateUser,
+    fetchEventInfo,
+    setEventInfo
 }
--- service/eventInfoService.js
+++ service/eventInfoService.js
@@ -1,21 +1,15 @@
 const {validateToken} = require("./authService");
-
-let eventInfo = {
-    name: "Event Name",
-    description: "Event Description",
-    organizer: "Organizer Name",
-    latitude: 40.233845,
-    longitude: -111.658531
-}
+const {fetchEventInfo, setEventInfo} = require("./database");
 
 async function getEventInfo(token) {
     await validateToken(token);
-    return eventInfo;
+    return fetchEventInfo();
 }
 
 async function updateEventInfo(info, token) {
     await validateToken(token);
-    eventInfo = { ...eventInfo, ...info };
+    const eventInfo = await fetchEventInfo(); // fetch first
+    await setEventInfo({ ...eventInfo, ...info });
 }
 
 module.exports = {
